dde9920104e9d6053f3f18059d26fa5cecaaca9f
RomanosSrt <rpsarantidis@gmail.com>
2024-09-01 09:03:13 +0300

    Flow control test cases

--- Flow/Loops.java
+++ Flow/Loops.java
@@ -4,7 +4,9 @@ public class Loops {
     public static void main(String args[]) {
         String result = "1";
         int score = 10;
-        if ((score += 10) == 100)
+        boolean cond;
+        // if ((score += 10) == 100)
+        if (cond = true)
             result = "A";
         else if ((score += 50) == 100)
             result = "B";
